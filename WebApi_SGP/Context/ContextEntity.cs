dc596afe9e9eae1b30baccfdc9eb5d125a64aed2
Julianno Garcia <julianno@p21sistemas.com.br>
2019-05-13 14:25:07 -0300

    Ajustes

--- WebApi_SGP/Context/ContextEntity.cs
+++ WebApi_SGP/Context/ContextEntity.cs
@@ -24,7 +24,7 @@ namespace WebApi_SGP.Context
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=juliano\julianno;Database=SGP21;Trusted_Connection=True;User ID=sa;Password=garciajtc241188;");
+            optionsBuilder.UseSqlServer(@"Server=;Database=SGP21;Trusted_Connection=True;User ID=sa;Password=;");
         }
     }
 }
